00e6fb07ee93d5b3f93224ceaaf7c97956249865
LewayHackintosh <michael.wei@foxmail.com>
2024-12-06 09:00:55 +0800

    feat: 添加调试器

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import VConsole from 'vconsole'
+
+// 初始化虚拟控制台
+new VConsole()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
